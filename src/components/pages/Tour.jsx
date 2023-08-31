06d481e56592cb236ee25e501e1d017d9e5581f2
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-31 13:40:41 -0700

    creates modal for tour video

--- src/components/pages/Tour.jsx
+++ src/components/pages/Tour.jsx
@@ -2,20 +2,38 @@ import PageHeading from "../PageHeading";
 import FinalCallToAction from "../FinalCallToAction";
 
 import videoImage from "../../assets/Video.jpg";
+import tourVideo from "../../assets/videos/TourVideo.mp4";
 
 export default function Tour() {
+	function openModal() {
+		const modal = document.querySelector("#modal");
+		modal.showModal();
+	}
+
+	function closeModal() {
+		const modal = document.querySelector("#modal");
+		modal.close();
+	}
+
 	return (
 		<div>
 			<PageHeading
 				title="Tour"
 				subheading="Build your first machine learning model in ten minutes. No code or experience required."
 			/>
-			<div className="tour-video-container">
+			<div className="tour-video-container" onClick={openModal}>
 				<img
+					className="open-video"
 					src={videoImage}
 					alt="Learn more about Lobe by watching our video."
 				/>
 			</div>
+			<dialog className="modal" id="modal">
+				<video src={tourVideo} autoPlay muted controls />
+				<button className="close-button" onClick={closeModal}>
+					X
+				</button>
+			</dialog>
 			<FinalCallToAction />
 		</div>
 	);
