1aea54653ab09279d904f2e12d62eb68f0600ab8
Jayson Thompson <jaysonthompson922@gmail.com>
2023-08-31 17:12:03 -0700

    updated file paths for moved assets folder

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,97 +3,97 @@ const videoCardData = [
 		title: "Plant Species",
 		description:
 			"Train your app to distinguish between different species of plants to help people identify poisonous vegetation.",
-		videoLink: "../src/assets/videos/Plants.mp4",
+		videoLink: "../assets/videos/Plants.mp4",
 	},
 	{
 		title: "Hand Gestures",
 		description:
 			"Train your app to understand the movement of your hand to create new kinds of immersive user experiences.",
-		videoLink: "../src/assets/videos/HandCount.mp4",
+		videoLink: "../../assets/videos/HandCount.mp4",
 	},
 	{
 		title: "Personal Trainer",
 		description:
 			"Train your app to recognize different workout positions to create an automated personal trainer that counts your reps.",
-		videoLink: "../src/assets/videos/FeelTheBurn.mp4",
+		videoLink: "../../assets/videos/FeelTheBurn.mp4",
 	},
 	{
 		title: "Emotional Reactions",
 		description:
 			"Train your app to react to different expressions allowing people to send emoji reactions using just their face.",
-		videoLink: "../src/assets/videos/Laughing.mp4",
+		videoLink: "../../assets/videos/Laughing.mp4",
 	},
 	{
 		title: "Interactive Painting",
 		description:
 			"Train your app to respond with sounds and stories while kids draw to create engaging learning experiences.",
-		videoLink: "../src/assets/videos/PaintingWithMike.mp4",
+		videoLink: "../../assets/videos/PaintingWithMike.mp4",
 	},
 	{
 		title: "Safety Precautions",
 		description:
 			"Train your app to monitor when masks are being correctly worn to help encourage proper safety precautions.",
-		videoLink: "../src/assets/videos/MaskUp.mp4",
+		videoLink: "../../assets/videos/MaskUp.mp4",
 	},
 	{
 		title: "Whale Watching",
 		description:
 			"Train your app to detect marine life in tourist photos to help conservationists map and protect the ocean.",
-		videoLink: "../src/assets/videos/Whales.mp4",
+		videoLink: "../../assets/videos/Whales.mp4",
 	},
 	{
 		title: "Beehive Health",
 		description:
 			"Train your app to monitor the entrance of hives to help beekeepers track and analyze the health of their colonies.",
-		videoLink: "../src/assets/videos/Bees.mp4",
+		videoLink: "../../assets/videos/Bees.mp4",
 	},
 	{
 		title: "Baby Monitor",
 		description:
 			"Train your app to detect when babies are sleeping or awake to notify parents before they start crying.",
-		videoLink: "../src/assets/videos/Sleeping.mp4",
+		videoLink: "../../assets/videos/Sleeping.mp4",
 	},
 	{
 		title: "Smart Checkout",
 		description:
 			"Train your app to recognize different types of produce to help checkout faster without the need to remember price look-up codes.",
-		videoLink: "../src/assets/videos/Checkout.mp4",
+		videoLink: "../../assets/videos/Checkout.mp4",
 	},
 	{
 		title: "Fire Watch",
 		description:
 			"Train your app to detect smoke and flames using remote cameras to help spot new wildfires before it's too late.",
-		videoLink: "../src/assets/videos/Wildfire.mp4",
+		videoLink: "../../assets/videos/Wildfire.mp4",
 	},
 	{
 		title: "Scientific Research",
 		description:
 			"Train your app to detect organisms under a microscope to help researchers analyze large quantities of scientific samples.",
-		videoLink: "../src/assets/videos/Microscope.mp4",
+		videoLink: "../../assets/videos/Microscope.mp4",
 	},
 	{
 		title: "Wildlife Behavior",
 		description:
 			"Train your app to detect wildlife behavior with remote cameras to track population and migration patterns over a large area.",
-		videoLink: "../src/assets/videos/AfricanWildlife.mp4",
+		videoLink: "../../assets/videos/AfricanWildlife.mp4",
 	},
 	{
 		title: "Equipment Analytics",
 		description:
 			"Train your app to monitor analog gauges and equipment to provide modern digital readouts and real time analytics.",
-		videoLink: "../src/assets/videos/Compression.mp4",
+		videoLink: "../../assets/videos/Compression.mp4",
 	},
 	{
 		title: "Telescopic Imagery",
 		description:
 			"Train your app to recognize different celestial bodies to help astronomers analyze large amounts of telescopic imagery.",
-		videoLink: "../src/assets/videos/Telescope.mp4",
+		videoLink: "../../assets/videos/Telescope.mp4",
 	},
 	{
 		title: "Aerial Imagery",
 		description:
 			"Train your app to monitor aerial imagery to gain insights into macro level changes happening around the globe.",
-		videoLink: "../src/assets/videos/Deforestation.mp4",
+		videoLink: "../../assets/videos/Deforestation.mp4",
 	},
 ];
 
