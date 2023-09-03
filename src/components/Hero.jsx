5c052b54be0a59e9479b76b0d285462b3214535c
Jayson Thompson <jaysonthompson922@gmail.com>
2023-09-03 05:23:43 -0700

    hero adjustments and title interval

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,29 +5,30 @@ import VideoModal from "./VideoModal";
 import { openModal } from "../utils";
 
 import videoTour from "../assets/videos/TourVideo.mp4";
+import homeVideo from "../assets/videos/Home.mp4";
 
 export default function Hero() {
 	const titleArray = [
 		"indentify plants",
 		"see gestures",
 		"count reps",
-		"feel emotioins",
+		"feel emotions",
 		"sense colors",
 		"check safety",
 	];
-
-	const titleLoop = titleArray.map((title) => {
-		setTimeout(() => {
-			return title;
-		}, 500);
-	});
-	console.log(titleLoop);
-	console.log(titleArray);
+	let i = 1;
+	setInterval(() => {
+		document.querySelector(".alternating-text").textContent = titleArray[i];
+		i = ++i % titleArray.length;
+	}, 4950);
 
 	return (
 		<section className="hero-container">
 			<div className="heading">
-				<h1 className="title">Train apps to {titleLoop}</h1>
+				<h1 className="title">
+					Train apps to <br />
+					<span className="alternating-text">indentify plants</span>
+				</h1>
 				<p className="subheading">
 					Lobe helps you train machine learning models with a free,
 					easy to use tool.
@@ -43,6 +44,7 @@ export default function Hero() {
 				</div>
 				<VideoModal videoFile={videoTour} />
 			</div>
+			<video src={homeVideo} autoPlay loop></video>
 		</section>
 	);
 }
