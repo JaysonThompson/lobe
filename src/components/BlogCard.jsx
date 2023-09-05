fb87b80d15e490494574ec5ae21f193800bf8f2c
Jayson Thompson <jaysonthompson922@gmail.com>
2023-09-04 21:31:22 -0700

    blog style corrections

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -6,7 +6,7 @@ function BlogCard({ title, intro, image, releaseDate }) {
 			<img src={image} alt={title} className="blog-image" />
 			<div className="blog-card-content">
 				<h2 className="title">{title}</h2>
-				<p className="description">{intro}</p>
+				<p className="intro">{intro}</p>
 				<p className="release-date">Released: {releaseDate}</p>
 			</div>
 		</div>
